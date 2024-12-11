56466c1d874077050f2fc97a23ae742cf3c6f479
iandresw <rbejarano@unah.hn>
2024-12-10 21:32:52 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Producto from './component/Producto.jsx'
-import Suma from './component/Suma.jsx';
+
 import Empleado from './component/Empleados.jsx'
 function App() {
   return (
